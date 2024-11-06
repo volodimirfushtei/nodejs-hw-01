b2dc49ad01536e41ce45c2ba6dcd205ac83d4d4a
Volodimir <161057867+volodimirfushtei@users.noreply.github.com>
2024-11-06 02:09:38 +0200

    add contacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,11 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
-
 import fs from 'node:fs/promises';
+
 export const writeContacts = async (updatedContacts) => {
   try {
-    await fs.promises.writeFile(
+    await fs.writeFile(
       PATH_DB,
       JSON.stringify(updatedContacts, null, 2),
+      'utf8',
     );
     console.log('Contacts updated successfully');
     return updatedContacts;
